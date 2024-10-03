69e284ca88501bc2cef18dc110506e853c304e54
luelD <cagaylueldawn@gmail.com>
2024-10-03 14:23:38 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log ("Luel Dawn");
+//Input
+const person1 ={
+    name:"Ricone Ometer",
+    age:15,
+    occupation:"Wala"
+    motto:"If you dont take a risk, you cant create Future"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+console.log("Motto: "+person1.motto);
